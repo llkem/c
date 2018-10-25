ce763b1c1ce4874aa3bd938008b0d347b19a8d55
Ryan <inventorryan@gmail.com>
2018-10-25 12:46:47 -0700

    Interactiveness!

---
+++ interact.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//getting user input
+
+int megl;
+
+int main(){
+printf("type something!\n");
+megl = getchar();
+printf("you entered: ");
+putchar(megl);
+printf("\n");
+return 0;
+}
---
+++ yeet.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+float number = 3.62;
+int main(){
+	printf("number is %f\n",number);
+	return 0;
+}
